62393214dbc8e8ee2a25f71eda2c5c0d58fdefaf
Mack Grissom <mcintyregrissom@gmail.com>
2024-02-01 14:45:31 +0700

    initial commit

---
+++ src/app/site/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+<>
+    <section className="h-full w-full pt-36 relative flex items-center justify-center flex-col">
+    <div className="absolute top-0 z-[-2] h-screen w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]"/>
+<p>Streamline your Business</p>
+    </section>
+
+
+
+    
+    </>
+  );
+}
