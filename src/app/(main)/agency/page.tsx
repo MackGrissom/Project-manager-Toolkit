36a7a946425951c7c2fc8eb4e2cbd3cde3d4ff3d
Mack Grissom <mcintyregrissom@gmail.com>
2024-02-05 09:35:03 +0700

    updated queries

--- src/app/(main)/agency/page.tsx
+++ src/app/(main)/agency/page.tsx
@@ -1,6 +1,16 @@
+import { getAuthUserDetails } from '@/lib/queries'
+import { currentUser } from '@clerk/nextjs'
+import { redirect } from 'next/navigation'
 import React from 'react'
 
-const AgencyPage = () => {
+const AgencyPage = async () => {
+  const authUser = await currentUser()
+  if(!authUser) return redirect('sign-in')
+
+  const agencyId = await verifyAndAcceptInvitation
+
+  const user = await getAuthUserDetails()
+
   return (
     <div>AgencyPage</div>
   )
