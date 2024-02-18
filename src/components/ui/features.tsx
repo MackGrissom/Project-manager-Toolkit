881191c39b05c3a89c7fb10c7ed77174981e5881
Mack Grissom <mcintyregrissom@gmail.com>
2024-02-18 10:22:42 +0700

    added dark/light to features section

--- src/components/ui/features.tsx
+++ src/components/ui/features.tsx
@@ -28,21 +28,21 @@ const features = [
 
 export default function Features() {
   return (
-    <div className="overflow-hidden bg-none py-24 sm:py-32 text-white">
+    <div className="overflow-hidden bg-none py-24 sm:py-32 dark:text-white text-primary">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
           <div className="lg:pr-8 lg:pt-4">
             <div className="lg:max-w-lg">
-              <h2 className="text-white font-semibold leading-7">Manage Your Business</h2>
-              <p className="mt-2 text-3xl font-bold tracking-tight text-white-900 sm:text-4xl">All-In-One Agency Management Suite</p>
-              <p className="mt-6 text-lg leading-8 text-white-600">
+              <h2 className="text-primary font-semibold leading-7">Manage Your Business</h2>
+              <p className="mt-2 text-3xl font-bold tracking-tight text-white-900 sm:text-4xl dark:text-white">All-In-One Agency Management Suite</p>
+              <p className="mt-6 text-lg leading-8 text-black dark:text-white">
                 You don&apos;t need 7 different softwares and multiple paid subscriptions to manage your business - do it all here and save yourself the stress. Made for freelancers, by freelancers. 
               </p>
-              <dl className="mt-10 max-w-xl space-y-8 text-white leading-7 text-white-600 lg:max-w-none">
+              <dl className="mt-10 max-w-xl space-y-8 dark:text-white leading-7 text-white-600 lg:max-w-none text-black">
                 {features.map((feature) => (
-                  <div key={feature.name} className="relative pl-9">
-                    <dt className="inline font-semibold text-white-900">
-                      <feature.icon className="absolute left-1 top-1 h-5 w-5 text-white" aria-hidden="true" />
+                  <div key={feature.name} className="relative pl-9 ">
+                    <dt className="inline font-semibold text-primary">
+                      <feature.icon className="absolute left-1 top-1 h-5 w-5 text-primary" aria-hidden="true" />
                       {feature.name}
                     </dt>{' '}
                     <dd className="inline">{feature.description}</dd>
